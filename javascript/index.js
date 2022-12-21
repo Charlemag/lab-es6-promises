4f28e960050043be7a3809371bc3094026a5be4d
Charlemag <Charlemagne03@gmail.com>
2022-12-20 20:27:23 -0500

    commit

--- javascript/index.js
+++ javascript/index.js
@@ -28,13 +28,53 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction("mashedPotatoes", 0, (step1) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+    getInstruction("mashedPotatoes", 1, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+      getInstruction("mashedPotatoes", 2, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+        getInstruction("mashedPotatoes", 3, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+          getInstruction("mashedPotatoes", 4, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+            document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+            document.querySelector("#mashedPotatoes").innerHTML += '<li> Mashed potatoes are done! </li>'
+          }, (error) => console.log(error));
+        }, (error) => console.log(error));
+      }, (error) => console.log(error));
+    }, (error) => console.log(error));
+}, (error) => console.log(error));
 
+ 
 // Iteration 2 - using promises
-// ...
+obtainInstruction('steak', 0)
+  .then( (result) => {
+    document.querySelector("#steak").innerHTML += `<li>${result}</li>`
+    return obtainInstruction('steak', 1)
+  })
+  .then((result2) => {
+    document.querySelector("#steak").innerHTML += `<li>${result2}</li>`
+    return obtainInstruction('steak', 2)
+  })
+  .then((result3) =>  {
+    document.querySelector("#steak").innerHTML += `<li>${result3}</li>`
+    return obtainInstruction('steak', 3)
+  })
+  .then((result4) => {
+    document.querySelector("#steak").innerHTML += `<li>${result3}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  
 
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+
+}
+
+
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
